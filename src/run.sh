b7492ee9b55fe5294d6d73484b5d79b12c5cb6d7
bodsul <bodsul@gmail.com>
2020-02-17 09:55:21 -0800

    change param

--- src/run.sh
+++ src/run.sh
@@ -2,7 +2,7 @@
 --max_iterations_ransac 100 \
 --distance_tolerance_ransac 0.5 \
 --cluster_tolerance_euclidean 1 \
---min_cluster_size_euclidean 50 \
+--min_cluster_size_euclidean 30 \
 --max_cluster_size_euclidean 100 \
 --filter_resolution 0.3 \
 --roi_min -100,-5,-100 \
